79c6f66a5c6f84c4e102a08270f510de8366024f
John Santosuosso <john.santosuosso1@gmail.com>
2024-07-02 10:40:18 -0400

    add token creation to api keys controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :api_keys, only: [:create, :destroy, :index]
+  post '/api-keys', to: 'api_keys#create'
+  delete '/api-keys', to: 'api_keys#destroy'
+  get '/api-keys', to: 'api_keys#index'
 end
